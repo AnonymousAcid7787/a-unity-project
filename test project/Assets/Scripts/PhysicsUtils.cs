863a7f94100777e08caebead2184e39659f34a40
AnonymousAcid <anonymousacid14@gmail.com>
2023-04-15 22:19:59 -0700

    trying to add physics with player movement

---
+++ test project/Assets/Scripts/PhysicsUtils.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics.Systems;
+using UnityEngine;
+
+public class PhysicsUtils
+{
+    // public static Entity Raycast(float3 startPos, float3 endPos) {
+        
+    // }
+}
--- test project/Assets/Scripts/PlayerInput/Systems/PlayerInputSystem.cs
+++ test project/Assets/Scripts/PlayerInput/Systems/PlayerInputSystem.cs
@@ -3,19 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Transforms;
+using Unity.Physics;
+using Unity.Mathematics;
 using UnityEngine;
 
 public partial class PlayerInputSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref PlayerMovementData data, in PlayerInputKeys keys) => {
+        Entities.ForEach((ref PhysicsVelocity velocity, ref PlayerMovementData data,  in PlayerInputKeys keys) => {
             int xDirection = Convert.ToInt32(Input.GetKey(keys.rightKey)) - Convert.ToInt32(Input.GetKey(keys.leftKey));
             int zDirection = Convert.ToInt32(Input.GetKey(keys.upKey)) - Convert.ToInt32(Input.GetKey(keys.downKey));
-            data.movementDirection.x = xDirection;
-            data.movementDirection.z = zDirection;
 
+            velocity.Linear.x = xDirection * data.movementSpeed;
+            velocity.Linear.z = zDirection * data.movementSpeed;
+            
             data.jump = Input.GetKey(keys.jump);
+
+            if(data.jump)
+                velocity.Linear.y = 1;
         }).WithoutBurst().Run();
     }
 }
