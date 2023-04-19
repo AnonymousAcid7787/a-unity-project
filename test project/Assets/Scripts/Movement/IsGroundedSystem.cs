d867db3c39a4a7dbbc1cdd15c89ac1c1e832d178
AnonymousAcid <anonymousacid14@gmail.com>
2023-04-19 12:47:50 -0700

    Fix collision event job
    TODO: do isGrounded check

--- test project/Assets/Scripts/Movement/IsGroundedSystem.cs
+++ test project/Assets/Scripts/Movement/IsGroundedSystem.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using UnityEngine;
 
+[RequireMatchingQueriesForUpdate]
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateAfter(typeof(PhysicsSystemGroup))]
 public partial struct IsGroundedSystem : ISystem {
-    private static SimulationSingleton simulationSingleton;
-    private static bool getSingleton;
     public void OnCreate(ref SystemState state) {
 
     }
@@ -16,43 +18,16 @@ public partial struct IsGroundedSystem : ISystem {
 
     }
     public void OnUpdate(ref SystemState state) {
-        if(!getSingleton) {
-            simulationSingleton = SystemAPI.GetSingleton<SimulationSingleton>();
-            getSingleton = true;
-        }
-        JobHandle handle = new IsGroundedJob{}.Schedule(simulationSingleton, state.Dependency);
-        handle.Complete();
+
+        state.Dependency = 
+            new IsGroundedJob{
+                
+            }.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
     }
-}
 
-// public partial struct GroundCheckSystem : ISystem
-// {
-//     private SimulationSingleton simulationSingleton;
-//     private bool getSingleton;
-
-//     public void OnCreate(ref SystemState state)
-//     {
-        
-//     }
-
-//     public void OnUpdate(ref SystemState state) {
-//         if(!getSingleton) {
-//             simulationSingleton = SystemAPI.GetSingleton<SimulationSingleton>();
-//             getSingleton = true;
-//         }
-//         JobHandle handle = new IsGroundedJob{}.Schedule(simulationSingleton, state.Dependency);
-//         handle.Complete();
-//     }
-
-//     public void OnDestroy(ref SystemState state) {}
-// }
-
-public partial struct IsGroundedJob : ICollisionEventsJob {
-    public void Execute(CollisionEvent collisionEvent) {
-        // if(collisionEvent.Normal.y < 0)
-        //     Debug.Log("on ground");
-        // else
-        //     Debug.Log("not on ground");
-        Debug.Log("test");
+    struct IsGroundedJob : ICollisionEventsJob {
+        public void Execute(CollisionEvent collisionEvent) {
+            // Debug.Log(collisionEvent.Normal);
+        }
     }
 }
