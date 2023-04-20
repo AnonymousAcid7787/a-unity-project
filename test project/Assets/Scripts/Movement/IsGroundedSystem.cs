64be0a71f4cbf29d1d4bd8b98f0344f13ba0ae9c
AnonymousAcid <anonymousacid14@gmail.com>
2023-04-19 17:20:25 -0700

    Implement isGrounded checks

--- test project/Assets/Scripts/Movement/IsGroundedSystem.cs
+++ test project/Assets/Scripts/Movement/IsGroundedSystem.cs
@@ -11,19 +11,64 @@ using UnityEngine;
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 [UpdateAfter(typeof(PhysicsSystemGroup))]
 public partial struct IsGroundedSystem : ISystem {
-    public void OnCreate(ref SystemState state) {
+    
+    internal ComponentDataHandles componentDataHandles;
 
+    internal struct ComponentDataHandles
+    {
+        public ComponentLookup<MovementData> movementData;
+
+        public ComponentDataHandles(ref SystemState systemState)
+        {
+            movementData = systemState.GetComponentLookup<MovementData>(false);
+        }
+
+        public void Update(ref SystemState systemState)
+        {
+            movementData.Update(ref systemState);
+        }
     }
+    
+    public void OnCreate(ref SystemState state) {
+        componentDataHandles = new ComponentDataHandles(ref state);
+    }
+    
     public void OnDestroy(ref SystemState state) {
 
     }
+    
     public void OnUpdate(ref SystemState state) {
-        state.Dependency = new IsGroundedJob{}.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
-    }
-}
+        componentDataHandles.Update(ref state);
 
-public partial struct IsGroundedJob : ICollisionEventsJob {
-    public void Execute(CollisionEvent collisionEvent) {
-        Debug.Log(192839124);
+        //First reset the isGrounded bool.
+        JobHandle handle = new ResetIsGroundedJob{}.ScheduleParallel(state.Dependency);
+        handle.Complete();
+
+        //Then update the isGrounded values
+        state.Dependency = new IsGroundedJob{
+            movementData = componentDataHandles.movementData
+        }.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
+
+        //Why do this? because this way of ground checking uses collision events. If the player is in the middle of the air, you want to reset the isGrounded bool
+        //When they are in the air after their isGrounded got reset, then it won't update to being true/false, leaving it as "false" which is accurate.
+    }
+
+    partial struct ResetIsGroundedJob : IJobEntity {
+        public void Execute(ref MovementData movementData) {
+            movementData.isGrounded = false;
+        }
+    }
+
+    partial struct IsGroundedJob : ICollisionEventsJob {
+        public ComponentLookup<MovementData> movementData;
+            public void Execute(CollisionEvent collisionEvent) {
+                bool isGrounded = collisionEvent.Normal.y > 0;
+
+                //Set isGrounded bool
+                Entity entityA = collisionEvent.EntityA;
+                MovementData dataCopy = movementData[entityA];
+                    dataCopy.isGrounded = isGrounded;
+                movementData[entityA] = dataCopy;
+        }
     }
 }
