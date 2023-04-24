fe9f4998a660aff90cdb535f81047f7d001b7240
AnonymousAcid <anonymousacid14@gmail.com>
2023-04-24 10:30:36 -0700

    Remove unused system

--- test project/Assets/Scripts/PlayerInput/Systems/LockPlayerRotationSystem.cs
+++ test project/Assets/Scripts/PlayerInput/Systems/LockPlayerRotationSystem.cs
@@ -5,25 +5,25 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Burst;
 
-[BurstCompile]
+
 public partial struct LockPlayerRotationSystem : ISystem
 {
     public static bool lockPlayerXZRotation = true;
 
-    [BurstCompile]
+        
     public void OnCreate(ref SystemState state) {
         lockPlayerXZRotation = true;
     }
 
-    [BurstCompile]
+    
     public void OnUpdate(ref SystemState state) {
         state.Dependency = new LockPlayerRotationJob{}.ScheduleParallel(state.Dependency);
     }
 
-    [BurstCompile]
+    
     partial struct LockPlayerRotationJob : IJobEntity {
         
-        [BurstCompile]
+        
         public void Execute(ref LocalTransform localTransform, in PlayerInputData data) {
             if(lockPlayerXZRotation) {
                 localTransform.Rotation.value.x = 0;
