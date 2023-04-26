e77baa34e990c1484568094628a07a7a1a1794ac
AnonymousAcid <anonymousacid14@gmail.com>
2023-04-26 14:05:46 -0700

    Adding vertical movement (character controller)

--- test project/Assets/Scripts/CharacterController/Systems/CharacterControllerSystem.cs
+++ test project/Assets/Scripts/CharacterController/Systems/CharacterControllerSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
@@ -30,18 +31,47 @@ public partial struct CharacterControllerSystem : ISystem
         
         public void Execute(ref CharacterControllerComponent controller,
                             in LocalTransform localTransform,
-                            in ColliderAspect colliderAspect)
+                            in ColliderAspect colliderAspect,
+                            in PhysicsCollider collider)
         {
+            #region vertical movement
+            //Offset current position by a tiny bit in order to prevent unwanted collisions from being registered
+            //(the object will be hovering slightly over the floor because of this. The hovering is practiaclly not visible to the eye)
             float3 epsilon = new float3(0, Epsilon, 0) * -math.normalize(controller.Gravity);
             float3 currPos = localTransform.Position + epsilon;
+
             quaternion currRot = localTransform.Rotation;
-            float3 gravityVel = controller.Gravity * deltaTime * (controller.IsGrounded ? 0:1);
+
+            //Gravity velocity will be increased unless it hits the ground
+            float3 gravityVelocity = controller.Gravity * deltaTime * (controller.IsGrounded ? 0:1);
             float3 verticalVelocity = new float3();
-            float3 gravityVelocity = new float3();
             float3 jumpVelocity = new float3();
 
+            float3 totalVelocity = jumpVelocity + gravityVelocity;
+
+            //Cast collider from the current positon of the object, to its future position when gravity is added.
+            NativeList<ColliderCastHit> verticalHits = new NativeList<ColliderCastHit>(Allocator.Temp);
+            float3 toPos = currPos + totalVelocity;
+            PhysicsUtils.ColliderCastAll(
+                ref verticalHits,
+                in physicsWorldSingleton,
+                in currPos,
+                in toPos,
+                in collider
+            );
+            PhysicsUtils.TrimByEntity(ref verticalHits, colliderAspect.Entity); 
+
             
-            float3 totalVelocity = 
+            //If there were hits other than the entity its self
+            if(verticalHits.Length != 0) {
+                RigidTransform transform = new RigidTransform {
+                    pos = currPos+totalVelocity,
+                    rot = currRot
+                };
+            }
+            #endregion vertical movement
+
+
         }
     }
 }
--- test project/Assets/Scripts/PhysicsUtils.cs
+++ test project/Assets/Scripts/PhysicsUtils.cs
@@ -1,13 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Physics.Systems;
+using Unity.Physics;
 using UnityEngine;
 
-public class PhysicsUtils
+public struct PhysicsUtils
 {
-    // public static Entity Raycast(float3 startPos, float3 endPos) {
+    public unsafe static void ColliderCastAll(
+        ref NativeList<ColliderCastHit> castHits,
+        in PhysicsWorldSingleton physicsWorldSingleton,
+        in float3 fromPos, in float3 toPos, in PhysicsCollider collider) 
+    {
+        unsafe {
+            physicsWorldSingleton.CastCollider(new ColliderCastInput {
+                Start = fromPos,
+                End = toPos,
+                Collider = collider.ColliderPtr
+            }, ref castHits); 
+        }
+    }
+
+    public unsafe static NativeList<DistanceHit> ColliderDistanceAll(
+        in PhysicsCollider collider,
+        float maxDistance,
+        in RigidTransform transform,
+        in PhysicsWorldSingleton physicsWorldSingleton,
+        Entity ignore,
+        Allocator allocator = Allocator.TempJob)
+    {
+        ColliderDistanceInput input = new ColliderDistanceInput {
+            Collider = collider.ColliderPtr,
+            MaxDistance = maxDistance,
+            Transform = transform
+        };
+
+        NativeList<DistanceHit> allDistances = new NativeList<DistanceHit>(allocator);
+
+        if(physicsWorldSingleton.CalculateDistance(input, ref allDistances)) {
+            TrimByEntity(ref allDistances, ignore);
+        }
+        return allDistances;
+    }
+
+    //Remove an entity if it is in the castResults list
+    public static void TrimByEntity<T>(ref NativeList<T> castResults, Entity ignore) where T : unmanaged, IQueryResult {
+        if (ignore == Entity.Null) {
+            return;
+        }
+
+        for (int i = (castResults.Length - 1); i >= 0; --i) {
+            if (ignore == castResults[i].Entity) {
+                castResults.RemoveAt(i);
+            }
+        }
+    }
+    
+    public unsafe static bool ColliderCast(
+        out ColliderCastHit nearestHit,
+        in PhysicsCollider collider,
+        float3 from,
+        float3 to,
+        ref PhysicsWorldSingleton physicsWorldSingleton,
+        Entity ignore,
+        CollisionFilter? filter = null,
+        EntityManager? manager = null,
+        ComponentLookup<PhysicsCollider>? colliderData = null,
+        Allocator allocator = Allocator.TempJob)
+    {
+        nearestHit = new ColliderCastHit();
+        NativeList<ColliderCastHit> allHits = new NativeList<ColliderCastHit>(Allocator.Temp);
+            ColliderCastAll(
+                ref allHits,
+                in physicsWorldSingleton,
+                from, to,
+                in collider
+            );
         
-    // }
+
+        if (filter.HasValue) {
+            if (manager.HasValue)
+                TrimByFilter(ref allHits, manager.Value, filter.Value);
+            
+            else if (colliderData.HasValue)
+                TrimByFilter(ref allHits, colliderData.Value, filter.Value);
+        }
+
+        GetSmallestFractional(ref allHits, out nearestHit);
+        allHits.Dispose();
+
+        return true;
+    }
+
+    public unsafe static void TrimByFilter<T>(ref NativeList<T> castResults, ComponentLookup<PhysicsCollider> colliderData, CollisionFilter filter) where T : unmanaged, IQueryResult {
+        for (int i = (castResults.Length - 1); i >= 0; --i) {
+            if (colliderData.HasComponent(castResults[i].Entity)) {
+                PhysicsCollider collider = colliderData[castResults[i].Entity];
+
+                if (CollisionFilter.IsCollisionEnabled(filter, collider.Value.Value.GetCollisionFilter()))
+                    continue;
+            }
+
+            castResults.RemoveAt(i);
+        }
+    }
+
+    public unsafe static void TrimByFilter<T>(ref NativeList<T> castResults, EntityManager entityManager, CollisionFilter filter) where T : unmanaged, IQueryResult {
+        for (int i = (castResults.Length - 1); i >= 0; --i) {
+            if (entityManager.HasComponent<PhysicsCollider>(castResults[i].Entity)) {
+                PhysicsCollider collider = entityManager.GetComponentData<PhysicsCollider>(castResults[i].Entity);
+
+                if (CollisionFilter.IsCollisionEnabled(filter, collider.Value.Value.GetCollisionFilter()))
+                    continue;
+            }
+
+            castResults.RemoveAt(i);
+        }
+    }
+
+    public static bool GetSmallestFractional<T>(ref NativeList<T> castResults, out T nearest) where T : unmanaged, IQueryResult {
+        nearest = default(T);
+
+        if (castResults.Length == 0)
+            return false;
+
+        float smallest = float.MaxValue;
+
+        for (int i = 0; i < castResults.Length; ++i) {
+            if (castResults[i].Fraction < smallest) {
+                smallest = castResults[i].Fraction;
+                nearest = castResults[i];
+            }
+        }
+
+        return true;
+    }
 }
