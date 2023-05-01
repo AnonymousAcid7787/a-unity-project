b0ca64ad676d0198a69c42539fc6bc244dc96163
AnonymousAcid <anonymousacid14@gmail.com>
2023-05-01 14:06:16 -0700

    Attempting to re-do jumping

--- test project/Assets/Scripts/Movement/Systems/IsGroundedSystem.cs
+++ test project/Assets/Scripts/Movement/Systems/IsGroundedSystem.cs
@@ -2,52 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Physics;
+using Unity.Physics.Aspects;
 using Unity.Physics.Systems;
+using Unity.Transforms;
 using UnityEngine;
 
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 [UpdateAfter(typeof(PhysicsSystemGroup))]
 [BurstCompile]
 public partial struct IsGroundedSystem : ISystem {
-    internal ComponentDataHandles componentDataHandles;
-
-    [BurstCompile]
-    internal struct ComponentDataHandles
-    {
-        public ComponentLookup<MovementData> movementData;
-
-        public ComponentDataHandles(ref SystemState systemState)
-        {
-            movementData = systemState.GetComponentLookup<MovementData>(false);
-        }
-
-        public void Update(ref SystemState systemState)
-        {
-            movementData.Update(ref systemState);
-        }
-    }
     
     [BurstCompile]
     public void OnCreate(ref SystemState state) {
-        componentDataHandles = new ComponentDataHandles(ref state);
+        
     }
     
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        return;
-        componentDataHandles.Update(ref state);
-
-        //First reset the isGrounded bool.
+        //First reset the isGrounded booleans.
         JobHandle handle = new ResetIsGroundedJob{}.ScheduleParallel(state.Dependency);
         handle.Complete();
 
         //Then update the isGrounded values
+        PhysicsWorldSingleton physicsWorldSingleton = SystemAPI.GetSingleton<PhysicsWorldSingleton>();
         state.Dependency = new IsGroundedJob{
-            // movementData = componentDataHandles.movementData
-        }.Schedule(/*SystemAPI.GetSingleton<SimulationSingleton>(),*/ state.Dependency);
+            physicsWorldSingleton = physicsWorldSingleton,
+        }.Schedule(state.Dependency);
 
         //Why do this? because this way of ground checking uses collision events. If the player is in the middle of the air, you want to reset the isGrounded bool
         //When they are in the air after their isGrounded got reset, then it won't update to being true/false, leaving it as "false" which is accurate.
@@ -55,38 +40,38 @@ public partial struct IsGroundedSystem : ISystem {
 
     [BurstCompile]
     partial struct ResetIsGroundedJob : IJobEntity {
-        
         [BurstCompile]
         public void Execute(ref MovementData movementData) {
             movementData.isGrounded = false;
         }
     }
-
-    #region commented
-    // [BurstCompile]
-    // partial struct IsGroundedJob : ICollisionEventsJob {
-        
-    //     public ComponentLookup<MovementData> movementData;
-
-    //     [BurstCompile]
-    //     public void Execute(CollisionEvent collisionEvent) {
-    //         Entity entityA = collisionEvent.EntityA;
-    //         if(!movementData.HasComponent(entityA))
-    //             return;
-    //         bool isGrounded = collisionEvent.Normal.y > 0;
-
-    //         //Set isGrounded bool
-    //         MovementData dataCopy = movementData[entityA];
-    //             dataCopy.isGrounded = isGrounded;
-    //         movementData[entityA] = dataCopy;
-    //     }
-    // }
-    #endregion commented
     
     [BurstCompile]
     partial struct IsGroundedJob : IJobEntity {
-        public void Execute(in PhysicsCollider col) {
+        public PhysicsWorldSingleton physicsWorldSingleton;
+        public void Execute(in PhysicsCollider collider,
+                            in LocalTransform localTransform,
+                            in ColliderAspect aspect,
+                            ref MovementData movementData) {
+            float3 pos = localTransform.Position - new float3(0, 0.1f, 0);
+            NativeList<ColliderCastHit> hits = new NativeList<ColliderCastHit>(Allocator.Temp);
+            unsafe {
+                //right now its just casting to its self
+                physicsWorldSingleton.CastCollider(
+                    new ColliderCastInput {
+                        Collider = collider.ColliderPtr,
+                        Start = pos,
+                        End = pos,
+                        Orientation = localTransform.Rotation,
+                        QueryColliderScale = localTransform.Scale-0.01f,
+                    },
+                    ref hits);
+            }
 
+            PhysicsUtils.RemoveEntityFromList(ref hits, aspect.Entity);
+            
+            movementData.isGrounded = hits.Length != 0;
+            hits.Dispose();
         }
     }
 
--- test project/Assets/Scripts/PhysicsUtils.cs
+++ test project/Assets/Scripts/PhysicsUtils.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Physics.Systems;
 using UnityEngine;
 
-public class PhysicsUtils
+public struct PhysicsUtils
 {
-    // public static Entity Raycast(float3 startPos, float3 endPos) {
-        
-    // }
+    public static void RemoveEntityFromList(ref NativeList<ColliderCastHit> hits, in Entity entityToIgnore)  {
+        for(var i=0; i<hits.Length; i++) {
+            if(hits[i].Entity == entityToIgnore) 
+                hits.RemoveAt(i);
+        }
+    }
 }
