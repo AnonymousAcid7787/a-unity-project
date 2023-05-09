b652d55b85ab87b11d211b7740695f4ca41b5ae1
AnonymousAcid <anonymousacid14@gmail.com>
2023-05-09 08:28:14 -0700

    Fix area of mesh to fit inputted xSize * zSize

--- test project/Assets/Scripts/TerrainGeneration/TerrainGenAuth.cs
+++ test project/Assets/Scripts/TerrainGeneration/TerrainGenAuth.cs
@@ -7,20 +7,24 @@ using UnityEngine;
 
 public class TerrainGenAuth : MonoBehaviour 
 {
-    public int xSize;
-    public int zSize;
+    public int xSize = 20;
+    public int zSize = 20;
 }
 
 public class TerrainGenBaker : Baker<TerrainGenAuth>
 {
     public override void Bake(TerrainGenAuth authoring)
     {
-        int vertexCount = (authoring.xSize + 1) * (authoring.zSize + 1);
+        int xSize = authoring.xSize - 1;
+        int zSize = authoring.zSize - 1;
+
+        int vertexCount = (xSize + 1) * (zSize + 1);
+        Debug.Log(vertexCount);
         Vector3[] vertices = new Vector3[vertexCount];
         
         //Vertices
-        for(int i = 0,z = 0; z <= authoring.zSize; z++) {
-            for(int x = 0; x <= authoring.xSize; x++) {
+        for(int i = 0,z = 0; z <= zSize; z++) {
+            for(int x = 0; x <= xSize; x++) {
                 float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2f;
                 vertices[i] = new float3(x, y, z);
                 i++;
@@ -30,16 +34,16 @@ public class TerrainGenBaker : Baker<TerrainGenAuth>
         //Triangles
         int vert = 0;
         int tris = 0;
-        int triangleCount = authoring.xSize * authoring.zSize * 6;
+        int triangleCount = xSize * zSize * 6;
         int[] triangles = new int[triangleCount];
-        for (int z = 0; z < authoring.zSize; z++) {
-            for(int x = 0; x < authoring.xSize; x++) {
+        for (int z = 0; z < zSize; z++) {
+            for(int x = 0; x < xSize; x++) {
                 triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + authoring.xSize + 1;
+                triangles[tris + 1] = vert + xSize + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + authoring.xSize + 1;
-                triangles[tris + 5] = vert + authoring.xSize + 2;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
 
                 vert++;
                 tris += 6;
