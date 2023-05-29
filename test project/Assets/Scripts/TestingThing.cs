d6c0bb8c0d5eaf43d6573dd5d1a7dd3c885050c4
AnonymousAcid <anonymousacid14@gmail.com>
2023-05-29 16:41:36 -0700

    add test Isystem

--- test project/Assets/Scripts/TestingThing.cs
+++ test project/Assets/Scripts/TestingThing.cs
@@ -5,43 +5,33 @@ using Unity.Entities;
 
 public class TestingThing : MonoBehaviour
 {
-    public Mesh mesh;
+    
 }
 
 public class TestBaker : Baker<TestingThing>
 {
     public override void Bake(TestingThing authoring)
     {
-        authoring.gameObject.GetComponent<MeshFilter>().mesh =
-            authoring.mesh;
+        
     }
 }
 
-public partial class TestSystem : SystemBase
-{
-    // private static bool foundSingleton;
-    protected override void OnCreate() {
-        // foundSingleton = false;
+public partial struct TestSystem : ISystem {
+    public void OnCreate(ref SystemState state) {
+
     }
 
-    protected override void OnUpdate()
-    {
-        // if(foundSingleton)
-        //     return;
-        // if(!SystemAPI.HasSingleton<RandomComponent>())
-        //     return;
+    public void OnDestroy(ref SystemState state) {
         
-        // foundSingleton = true;
-        // uint seed = SystemAPI.GetSingletonRW<RandomComponent>().ValueRW.random.state;
-        // int[,] grid = TerrainGenUtils.DiamondSquare(129, 2, 1, 28, new Unity.Mathematics.Random(seed));
-        // string str = "";
-        // for(var y = 0; y < xSize; y++) {
-        //     for(var x = 0; x < zSize; x++) {
-        //         str += grid[y, x] + ",";
-        //     }
-        //     str += "\n";
-        // }
-        // GUIUtility.systemCopyBuffer = str;
-        // Debug.Log("Done");
+    }
+
+    public void OnUpdate(ref SystemState state) {
+        state.Dependency = new TestJob{}.ScheduleParallel(state.Dependency);
+    }
+
+    partial struct TestJob : IJobEntity {
+        public void Execute(in Entity entity, in TerrainGeneratorComponent terrainGenCmp) {
+            Debug.Log(entity.ToString());
+        }
     }
 }
