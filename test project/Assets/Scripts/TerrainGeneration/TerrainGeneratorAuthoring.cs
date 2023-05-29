6893c450bf4dfdaee1619cbb85521ee49378b75a
AnonymousAcid <anonymousacid14@gmail.com>
2023-05-29 14:18:58 -0700

    (maybe) got the fractal noise voxel thing done

--- test project/Assets/Scripts/TerrainGeneration/TerrainGeneratorAuthoring.cs
+++ test project/Assets/Scripts/TerrainGeneration/TerrainGeneratorAuthoring.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Collections;
 
 public class TerrainGeneratorAuthoring : MonoBehaviour
 {
-    public int2 chunkPosition;
-    public int chunkWidth = 10;
-    public int chunkHeight = 10;
+    public int3 chunkPosition;
+    public int chunkSize = 10;
     public int minHeight = 1;
     public int maxHeight = 8;
     public float frequency;
@@ -21,11 +21,13 @@ public class TerrainGeneratorBaker : Baker<TerrainGeneratorAuthoring>
 {
     public override void Bake(TerrainGeneratorAuthoring authoring)
     {
-        int xSize = authoring.chunkWidth;
-        int zSize = authoring.chunkHeight;
-        float[,] noiseGrid = FractalNoise(
+        int chunkSize = authoring.chunkSize;
+
+        int[,,] voxelMap = new int[chunkSize, chunkSize, chunkSize];
+
+        int[,] noiseGrid = FractalNoiseInt(
             authoring.chunkPosition.x, authoring.chunkPosition.y,
-            authoring.chunkWidth, authoring.chunkHeight,
+            chunkSize, chunkSize,
             authoring.minHeight, authoring.maxHeight,
             authoring.frequency,
             authoring.octaves,
@@ -33,16 +35,22 @@ public class TerrainGeneratorBaker : Baker<TerrainGeneratorAuthoring>
             authoring.persistence
         );
 
-        //loop through noiseGrid
-        //the current x and y value will be the x and z value of the voxels
-        //the value of the cells in the noiseGrid will be the y value
+        for(var x = 0 ; x < chunkSize; x ++) {
+            for(var z = 0 ; z < chunkSize; z ++) {
+                //"create" voxels at the heights specified by the noiseGrid.
+                voxelMap[x, noiseGrid[z,x], z] = 1;
+            }
+        }
+        
+        int arrayLength = chunkSize*chunkSize*chunkSize;
+        int[] flatArrayManaged = Utils.Flatten3DArray(voxelMap, chunkSize, chunkSize, chunkSize);
+        NativeArray<int> voxelMapFlattened = new NativeArray<int>(flatArrayManaged, Allocator.Persistent);
 
-        // Mesh mesh = new Mesh();
-        // mesh.vertices = vertices;
-        // mesh.triangles = triangles;
-        // mesh.RecalculateBounds();
-        // mesh.RecalculateNormals();
-        // authoring.gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        AddComponent(GetEntity(TransformUsageFlags.None), new TerrainGeneratorComponent{
+            chunkSize = chunkSize,
+            mapSize = chunkSize,
+            voxelMapFlattened = voxelMapFlattened
+        });
     }
     
     public static float[,] FractalNoise(int chunkX, int chunkY, int gridWidth, int gridHeight, int minHeight, int maxHeight, float frequency, int octaves, float lacunarity, float persistence) {
@@ -72,4 +80,36 @@ public class TerrainGeneratorBaker : Baker<TerrainGeneratorAuthoring>
 		return grid;
 	}
 
+    public static int[,] FractalNoiseInt(int chunkX, int chunkY, int gridWidth, int gridHeight, int minHeight, int maxHeight, float frequency, int octaves, float lacunarity, float persistence) {
+
+		int[,] grid = new int[gridHeight, gridWidth];
+		float amplitude = maxHeight/2f;
+
+		for(int y = chunkX; y < gridHeight; y++) {
+			for(int x = chunkY; x < gridWidth; x++) {
+				float cellElevation = amplitude;
+				float tFrequency = frequency;
+				float tAmplitude = amplitude;
+
+				for(int octave = 0; octave < octaves; octave++) {
+                    float2 sampleVec = new float2(x * tFrequency, y * tFrequency);
+					cellElevation += noise.snoise(sampleVec) * tAmplitude;
+
+					tFrequency *= lacunarity;
+					tAmplitude *= persistence;
+				}
+
+				cellElevation = Mathf.Clamp(cellElevation, minHeight, maxHeight);
+				grid[y, x] = (int)cellElevation;
+			}
+		}
+
+		return grid;
+	}
+}
+
+public struct TerrainGeneratorComponent : IComponentData {
+    public NativeArray<int> voxelMapFlattened;/*flattened 3D array*/
+    public int mapSize;
+    public int chunkSize;
 }
--- test project/Assets/Scripts/Utils/Utils.cs
+++ test project/Assets/Scripts/Utils/Utils.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public struct Utils
 {
-    
+    //inline this method if possible
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T[] Flatten3DArray<T>(T[,,] array3D, int w, int h, int d) {
+        T[] array = new T[w*h*d];
+        
+        for(int x = 0; x < w; x++){
+            for(int y = 0; y < h; y++){
+                for(int z = 0; z < d; z++){
+                    array[x + h * (y + d * z)] = array3D[x, y, z];
+                }
+            }
+        }
+
+        return array;
+    }
 }
